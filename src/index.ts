745bf921a690e4a1e74bd3484be0340b406a81ea
Miguel Carrasco Q <miguel.carrasco.quijada@gmail.com>
2024-02-13 17:39:02 -0300

    back to port 4000 (instead of 80)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import server from './server';
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 80 },
+  listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
